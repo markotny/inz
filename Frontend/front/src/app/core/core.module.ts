448ee8c00490ea5eaf98f006312cb334665fc32a
Marcin Kotas <markotny97@gmail.com>
2019-09-21 22:25:52 +0200

    Create auth-callback component

--- Frontend/front/src/app/core/core.module.ts
+++ Frontend/front/src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import {NgModule, Optional, SkipSelf} from '@angular/core';
 import {AuthService} from './authentication/auth.service';
 import {AuthGuard} from './authentication/auth.guard';
+import { AuthCallbackComponent } from './authentication/auth-callback/auth-callback.component';
 
 @NgModule({
   imports: [],
-  providers: [AuthService, AuthGuard]
+  providers: [AuthService, AuthGuard],
+  declarations: [AuthCallbackComponent]
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
