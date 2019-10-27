13413b46a0dfb3311897353a25dbd6dcdb242f6d
Marcin Kotas <markotny97@gmail.com>
2019-10-27 15:15:57 +0100

    change navigation-base subscribtion logic

--- SPA/src/app/shell/navigation-base/navigation-base.ts
+++ SPA/src/app/shell/navigation-base/navigation-base.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
+import {OnInit} from '@angular/core';
 import {AuthService} from '@core/authentication/auth.service';
-import {Subscription} from 'rxjs';
+import {Observable} from 'rxjs';
+import {tap, share} from 'rxjs/operators';
 
-export class NavigationBase implements OnInit, OnDestroy {
+export class NavigationBase implements OnInit {
 	exploreLinks = [
 		{name: 'New', route: '/home'},
 		{name: 'Top', route: '/explore'},
@@ -12,16 +13,21 @@ export class NavigationBase implements OnInit, OnDestroy {
 	accountLinks = [{name: 'Profile', route: '/home'}, {name: 'Settings', route: '/settings'}];
 
 	userName: string;
-	isAuthenticated: boolean;
-	subscription: Subscription;
+	isAuthenticated$: Observable<boolean>;
 
 	constructor(protected authService: AuthService) {}
 
 	ngOnInit() {
-		this.subscription = this.authService.authNavStatus$.subscribe(status => {
-			this.isAuthenticated = status;
-			this.userName = this.authService.name;
-		});
+		this.isAuthenticated$ = this.authService.authNavStatus$.pipe(
+			tap(status => {
+				if (status) {
+					this.userName = this.authService.name;
+				} else {
+					this.userName = 'anonymous';
+				}
+			}),
+			share()
+		);
 	}
 
 	register() {
@@ -34,9 +40,5 @@ export class NavigationBase implements OnInit, OnDestroy {
 
 	signout() {
 		this.authService.signout();
-	}
-
-	ngOnDestroy() {
-		this.subscription.unsubscribe();
 	}
 }
